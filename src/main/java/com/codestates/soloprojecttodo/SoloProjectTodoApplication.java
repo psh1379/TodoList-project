19fb9d59bf03378e7297d41bbe364b042c7cc76d
psh1379 <110961521+psh1379@users.noreply.github.com>
2022-12-12 10:37:29 +0900

    Add

---
+++ src/main/java/com/codestates/soloprojecttodo/SoloProjectTodoApplication.java
@@ -0,0 +1,13 @@
+package com.codestates.soloprojecttodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoloProjectTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoloProjectTodoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codestates/soloprojecttodo/SoloProjectTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codestates.soloprojecttodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoloProjectTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
